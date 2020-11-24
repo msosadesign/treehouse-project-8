8587c2b228e04dae7faad558245cf7f59d3b352b
Miguel Sosa <59977013+msosadesign@users.noreply.github.com>
2020-11-24 21:03:06 +0100

    Add popup with user data

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const randomUsersUrl =
   "https://randomuser.me/api/?results=12&nat=us,gb,es&inc=name,email,location,cell,dob,picture";
 const gridContainer = document.getElementsByClassName("grid-layout")[0];
+const modal = document.getElementById("modal");
+const closeOverlayButton = document.getElementById("close-overlay");
+
+class Person {
+  constructor(name, email, city, phone, address, birthday, img) {
+    this.name = name;
+    this.email = email;
+    this.city = city;
+    this.phone = phone;
+    this.address = address;
+    this.birthday = birthday;
+    this.img = img;
+  }
+}
 
 function checkStatus(res) {
   if (res.ok) {
@@ -16,36 +30,86 @@ async function fetchData(url) {
   return res.json();
 }
 
+function openPopup(userData) {
+  const name = document.getElementById("popup-name");
+  const email = document.getElementById("popup-email");
+  const city = document.getElementById("popup-city");
+  const phone = document.getElementById("popup-phone");
+  const birthday = document.getElementById("popup-birthday");
+  const address = document.getElementById("popup-address");
+  const img = document.getElementById("popup-img");
+
+  modal.style.display = "flex";
+
+  name.textContent = userData.name;
+  email.textContent = userData.email;
+  email.setAttribute("href", `mailto:${userData.email}`);
+  city.textContent = userData.city;
+  phone.textContent = userData.phone;
+  birthday.textContent = `Birthday: ${userData.birthday.getDate()}/${userData.birthday.getMonth()}/${userData.birthday.getFullYear()}`;
+  address.textContent = userData.address;
+  img.setAttribute("src", userData.img);
+  img.setAttribute("alt", `Picture of ${userData.name}`);
+}
+
 function insertUsers(data) {
   const usersArray = data.results;
-  console.log(usersArray);
 
-  usersArray.forEach((user) => {
-    const name = `${user.name.first} ${user.name.last}`;
-    const email = user.email;
-    const city = user.location.city;
-    const img = user.picture.large;
+  usersArray.forEach((userObj) => {
+    const name = `${userObj.name.first} ${userObj.name.last}`;
+    const email = userObj.email;
+    const city = userObj.location.city;
+    const phone = userObj.cell;
+    const address = `${userObj.location.street.number} ${userObj.location.street.name}, ${userObj.location.postcode}`;
+    const birthday = new Date(userObj.dob.date);
+    const img = userObj.picture.large;
+    const userData = new Person(
+      name,
+      email,
+      city,
+      phone,
+      address,
+      birthday,
+      img
+    );
 
-    const article = document.createElement("article");
-    article.className = "user-card";
+    const card = document.createElement("article");
+    card.className = "user-card";
 
-    article.innerHTML = `
+    card.innerHTML = `
     <img
-    src="${img}"
-    alt="Image of ${name}"
+    src="${userData.img}"
+    alt="Image of ${userData.name}"
     />
-    <h2>${name}</h2>
-    <a href="mailto:${email}">${email}</a>
-    <p>${city}</p>
+    <h2>${userData.name}</h2>
+    <a href="mailto:${userData.email}">${userData.email}</a>
+    <p>${userData.city}</p>
     `;
 
-    gridContainer.appendChild(article);
+    gridContainer.appendChild(card);
+
+    // Add event listener to open popup
+    card.addEventListener("click", () => openPopup(userData));
   });
 }
+
+// Close overlay
+
+const closeOverlay = () => (modal.style.display = "none");
+
+closeOverlayButton.addEventListener("click", closeOverlay);
+modal.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    closeOverlay();
+  }
+});
+
+// Fetch data!
 
 fetchData(randomUsersUrl)
   .then(insertUsers)
   .catch((err) => {
+    console.error(err);
     gridContainer.innerHTML = `<h2>Error fetching users, please reload the page.</h2>`;
     gridContainer.style.display = "block";
   });
